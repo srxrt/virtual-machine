b3a67f65d67419c3eeda6a2014f215a91d78af81
srxrt <str9703@gmail.com>
2024-09-10 11:10:48 +0900

    fix: saved the file

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdint.h>
+#define MEMORY_MAX(1<<16)
+typedef uint16_t uint16;
+
+uint16 memory[MEMORY_MAX];  // 65536 LOCATIONS IN RAM
+
+enum
+  {
+    R_R0 = 0,
+    R_R1,
+    R_R2,
+    R_R3,
+    R_R4,
+    R_R5,
+    R_R6,
+    R_R7,
+    R_PC, //PROGRAM COUNTER
+    R_COND,
+    R_COUNT
+  };
+
+uint16 reg[R_COUNT]; // registers
+
+
+
+
+
